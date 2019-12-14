b1a46039f2be948e82fd543e3bf133207beba3b5
xRollbacks <56001809+xRollbacks@users.noreply.github.com>
2019-12-14 17:33:25 -0300

    Commit para mostrar el codigo.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-$cuadro = document.querySelectorAll(".cuadro");
+const $cuadro = document.querySelectorAll(".cuadro");
+
+const imagenTapada = "src/imagenTapada.png";
 
 const imagenes = [
 	"src/a.jpg",
@@ -24,11 +26,48 @@ function ordenarArrayAlAzar() {
 	});
 }
 ordenarArrayAlAzar();
+crearImagenTapada();
 
-$cuadro.forEach(function($cuadro, i) {
-	imagen = document.createElement("img");
-	imagen.className += "rounded";
+function crearImagenTapada() {
+	$cuadro.forEach(function($cuadro, i) {
+		imagen = document.createElement("img");
+		imagen.className += "border";
+		imagen.className += "rounded";
+		imagen.className += "imagen"[i];
+		imagen.src = imagenTapada;
+		imagen.onclick = function() {
+			debugger;
+			if (imagen[i].src == "file:///D:/Github/MemoTest/src/imagenTapada.png") {
+				imagen[i].src = imagenes[i];
+				console.log("Entra");
+			} else {
+				console.log("No entra");
+			}
+		};
+		$cuadro.appendChild(imagen);
+	});
+}
+// const imagenReal = document.querySelectorAll(".imagen");
+// imagenReal.onclick = darVueltaImagen();
 
-	imagen.src = imagenes[i];
-	$cuadro.appendChild(imagen);
-});
+// function darVueltaImagen() {
+// 	debugger;
+// 	if (imagenReal.src == imagenTapada) {
+// 		imagenReal.src = imagenes[i];
+// 		console.log("123");
+// 	} else {
+// 		console.log("asd");
+// 	}
+
+// const imagenPregunta = document.querySelectorAll(".imagen");
+
+// function darVueltaImagen(imagen) {
+// 	imagenPregunta;
+// }
+
+// imagenPregunta.onclick = function() {
+// 	darVueltaImagen();
+// };
+// if (imagen.src == imagenTapada) {
+// 	imagen.src = imagenes[i];
+// }
